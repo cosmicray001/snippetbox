6e624db4fd8229e0eb297e97dc0093d44705e99d
cosmicray001 <samiulislambracu@gmail.com>
2023-06-01 00:15:57 +0600

    Managing configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":8000", "HTTP network address")
+	flag.Parse()
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -14,8 +17,8 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Println("Starting server on :8000")
-	err := http.ListenAndServe(":8000", mux)
+	log.Printf("Starting server on : %s\n", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
