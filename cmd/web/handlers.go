6f8acc664e6e2b91b7c7476625568825cdbaa3d3
cosmicray001 <samiulislambracu@gmail.com>
2023-06-05 10:00:23 +0600

    add: Centralized error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,7 +9,7 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	files := []string{
@@ -19,14 +19,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 	}
 }
 
@@ -34,7 +32,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	snippetID, err := strconv.Atoi(id)
 	if err != nil || snippetID < 1 {
-		http.Error(w, "Invalid Snippet ID", http.StatusNotFound)
+		app.notFound(w)
 		return
 	}
 	msg := fmt.Sprintf("Display a specific snippet with ID %d...", snippetID)
@@ -44,9 +42,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
-		//w.WriteHeader(http.StatusMethodNotAllowed)
-		//_, _ = w.Write([]byte("Method not allowed"))
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	_, _ = w.Write([]byte("Create a new snippet..."))
