6dbb482e42efed70ea085ea5b28c6dd7a5b2580e
cosmicray001 <samiulislambracu@gmail.com>
2023-06-08 23:56:58 +0600

    add: Usage in our web application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +16,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":8000", "HTTP network address")
+	dsn := flag.String("dsn", os.Getenv("DB_URL"), "MySQL data source name")
 	flag.Parse()
 	infoLogFile, err := os.OpenFile("./tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
@@ -27,6 +30,13 @@ func main() {
 	}
 	defer errorLogFile.Close()
 	errorLog := log.New(errorLogFile, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
@@ -41,4 +51,15 @@ func main() {
 	if err != nil {
 		errorLog.Fatal(err)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
