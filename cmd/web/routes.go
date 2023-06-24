94d59e1c04baf7250c009a423506b8b3d125b183
cosmicray001 <samiulislambracu@gmail.com>
2023-06-24 18:13:28 +0600

    Clean URLs and method-based routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,15 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 func (app *application) routers() http.Handler {
-	mux := http.NewServeMux()
+	router := httprouter.New()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", FileListenerDisable(fileServer)))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", FileListenerDisable(fileServer)))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
+
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	return standard.Then(router)
 }
