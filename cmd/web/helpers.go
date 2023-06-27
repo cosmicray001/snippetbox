9f278bf0129e5aa202e803a1dcd3467696871f38
cosmicray001 <samiulislambracu@gmail.com>
2023-06-27 21:23:53 +0600

    Working with session data

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -13,6 +13,7 @@ import (
 func (app *application) newTemplateDate(r *http.Request) *templateData {
 	return &templateData{
 		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
 	}
 }
 
